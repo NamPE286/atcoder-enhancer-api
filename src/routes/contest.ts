e608677e32edce34e406e7653e3639c93903a4a2
NamPE286 <nambuihung654@gmail.com>
2024-10-19 13:16:12 +0700

    file metadata

--- src/routes/contest.ts
+++ src/routes/contest.ts
@@ -9,11 +9,22 @@ async function fetchDropboxFile(path: string) {
         path: path,
         url: "https://www.dropbox.com/sh/nx3tnilzqz7df8a/AAAYlTq2tiEHl5hsESw6-yfLa?e=3&dl=0"
     })
-    // @ts-ignore
-    const fileContent: string = res.result.fileBinary.toString()
 
-    return fileContent
+    // @ts-ignore
+    return res.result.fileBinary.toString()
 }
+
+async function fetchDropboxFileMetadata(path: string) {
+    const a = 'https://www.dropbox.com/sh/nx3tnilzqz7df8a/AAAYlTq2tiEHl5hsESw6-yfLa?e=3&dl=0'
+    const res = await dbx.sharingGetSharedLinkMetadata({
+        path: path,
+        url: "https://www.dropbox.com/sh/nx3tnilzqz7df8a/AAAYlTq2tiEHl5hsESw6-yfLa?e=3&dl=0"
+    })
+
+    // @ts-ignore
+    return res.result.size
+}
+
 
 router.route('/:contest')
     .get(async (req, res) => {
@@ -53,10 +64,30 @@ router.route('/:contest/:problem/testcase/:testcase')
 
             res.send({ in: input, out: output })
         } catch {
-            console.log("b")
             try {
                 const input = await fetchDropboxFile(`/${contest.toUpperCase()}/${problem.toUpperCase()}/in/${testcase}`)
                 const output = await fetchDropboxFile(`/${contest.toUpperCase()}/${problem.toUpperCase()}/out/${testcase}`)
+
+                res.send({ in: input, out: output })
+            } catch {
+                res.status(404).send()
+            }
+        }
+    })
+
+router.route('/:contest/:problem/testcase/:testcase/metadata')
+    .get(async (req, res) => {
+        const { contest, problem, testcase } = req.params
+
+        try {
+            const input = await fetchDropboxFileMetadata(`/${contest}/${problem.toUpperCase()}/in/${testcase}`)
+            const output = await fetchDropboxFileMetadata(`/${contest}/${problem.toUpperCase()}/out/${testcase}`)
+
+            res.send({ in: input, out: output })
+        } catch {
+            try {
+                const input = await fetchDropboxFileMetadata(`/${contest.toUpperCase()}/${problem.toUpperCase()}/in/${testcase}`)
+                const output = await fetchDropboxFileMetadata(`/${contest.toUpperCase()}/${problem.toUpperCase()}/out/${testcase}`)
 
                 res.send({ in: input, out: output })
             } catch {
